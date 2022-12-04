a32639d13a9a57421b5b8dd405ab4abafa6569f5
Baburaj <94101371+babu-sbr@users.noreply.github.com>
2022-12-04 19:11:45 +0530

    Added a add() function
    It has variable length arguments to accept n number of arguments

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # Creating a simple program and push it in repo using git
 print("Git practice")
-a = 10
-b = 20
-print("Sum of {} and {} is {}".format(a,b,a+b))
+def add(*n):
+  return sum(n)
+print("Sum of {} and {} is {}".format(add(1,2)))
